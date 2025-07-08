3c2bbd6dbc7ce2569191fd40cbf0d189d9e63e0d
Oscar Calzada <noctaelux@gmail.com>
2025-07-08 15:56:48 -0600

    Introduce `ReactiveAPI` for streaming via SSE. Update logging level to `FINE` in application properties.

---
+++ src/main/java/com/example/api/ReactiveAPI.java
@@ -0,0 +1,39 @@
+package com.example.api;
+
+import io.smallrye.mutiny.Multi;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.RestStreamElementType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+
+@Path("/reactive")
+public class ReactiveAPI {
+
+    private final static Logger LOG = LoggerFactory.getLogger(ReactiveAPI.class);
+
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @RestStreamElementType(MediaType.TEXT_PLAIN)
+    @Path("/backpressure/{count}/{message}")
+    public Multi<String> stream(@PathParam("count") long count,@PathParam("message") String message) {
+
+        LOG.debug("Serving a request every {} nanoseconds.",count);
+
+        return Multi
+                .createFrom()
+                .ticks()
+                .every(Duration.ofNanos(count))
+                .onOverflow()
+                .buffer(1000)
+                .onItem()
+                .transform(n -> String.format("Hello %s - sending message # %d.", message, n));
+
+    }
+
+}
